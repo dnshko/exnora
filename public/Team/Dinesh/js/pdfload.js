d00cfb5e4ad442b2119b7dcbc7818a4a23f8b066
Dinesh <34399732+dnshko@users.noreply.github.com>
2020-11-09 16:54:15 +0530

    remove files

---
+++ public/Team/Dinesh/js/pdfload.js
@@ -0,0 +1,497 @@
+$(document).ready(function () {
+    $("#ma1").flipBook({
+        pdfUrl: "pdf/ma1.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma1.pdf",
+            name: "ma1.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma1.pdf",
+            name: "ma1.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma2").flipBook({
+        pdfUrl: "pdf/ma2.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma2.pdf",
+            name: "ma2.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma2.pdf",
+            name: "ma2.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma3").flipBook({
+        pdfUrl: "pdf/ma3.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma3.pdf",
+            name: "ma3.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma3.pdf",
+            name: "ma3.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma4").flipBook({
+        pdfUrl: "pdf/ma4.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma4.pdf",
+            name: "ma4.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma4.pdf",
+            name: "ma4.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma5").flipBook({
+        pdfUrl: "pdf/ma5.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma5.pdf",
+            name: "ma5.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma5.pdf",
+            name: "ma5.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma6").flipBook({
+        pdfUrl: "pdf/ma6.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma6.pdf",
+            name: "ma6.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma6.pdf",
+            name: "ma6.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
+$(document).ready(function () {
+    $("#ma7").flipBook({
+        pdfUrl: "pdf/ma7.pdf",
+        viewMode: 'swipe',
+        deeplinking: {
+            enabled: true
+        },
+        skin: 'dark',
+        layout: 4,
+        btnSound: {
+            vAlign: "top",
+            hAlign: "left"
+        },
+        btnSelect:{
+            enabled: false,
+        },
+        btnBookmark:{
+            enabled: false,
+        },
+        btnToc:{
+            enabled: false,
+        },
+        btnThumbs:{
+            enabled: false,
+        },
+        btnAutoplay: {
+            enabled: false,
+        },
+        currentPage: {
+            vAlign: "bottom",
+            hAlign: "left"
+        },
+        btnDownloadPages:{
+            enabled: true,
+            title: "Download pages",
+            icon: "fa-download",
+            icon2: "file_download",
+            url: "pdf/ma7.pdf",
+            name: "ma7.pdf"
+        },
+        btnDownloadPdf:{
+            enabled: true,
+            title: "Download PDF",
+            icon: "fa-download",
+            icon2: "picture_as_pdf",
+            openInNewWindow: true,
+            url: "pdf/ma7.pdf",
+            name: "ma7.pdf"
+        },
+        btnClose:{
+            title: "Close", 
+            icon: "fa-times", 
+            icon2: "close", 
+            hAlign:'left', 
+            vAlign:'top' 
+        },
+        btnPrint:{
+            enabled: false,
+        },
+        btnExpand:{
+            enabled: false,
+        },
+        lightBox: true,
+        lightboxBackground: 'rgb(219,244,248)',
+        // singlePageMode: true,
+        viewMode: "3d",
+        icons:'material',
+        
+    });
+
+})
